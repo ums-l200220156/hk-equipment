90ac8d2f5a2dae2267f13acdea73761cd2eff1d4
Alfian Candra Irwana <l200220156@student.ums.ac.id>
2026-03-10 18:33:56 +0700

    add payment_proof rentals

---
+++ database/migrations/2026_03_10_183308_add_payment_proof_to_rentals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasColumn('rentals', 'payment_proof')) {
+
+            Schema::table('rentals', function (Blueprint $table) {
+                $table->string('payment_proof', 255)
+                      ->nullable()
+                      ->after('payment_method');
+            });
+
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (Schema::hasColumn('rentals', 'payment_proof')) {
+
+            Schema::table('rentals', function (Blueprint $table) {
+                $table->dropColumn('payment_proof');
+            });
+
+        }
+    }
+};
